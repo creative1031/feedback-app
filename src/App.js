3eba590e9ff51220e922f0e2bcf64049d9543fb9
mocomocoafro <engineer@creative1031.com>
2022-05-12 22:30:52 +0900

    reset

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import { FeedbackProvider } from './context/FeedbackContext'
-import FeedbackList from './components/FeedbackList'
-
 function App() {
-  return (
-    <FeedbackProvider>
-      <FeedbackList />
-    </FeedbackProvider>
-  )
+  return <div>App</div>
 }
 
 export default App
